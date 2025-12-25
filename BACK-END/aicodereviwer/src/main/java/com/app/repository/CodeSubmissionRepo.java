ad90fca5f94adee7bfc4f451a967c40a1ebb512b
Pawan <kumarpawanm8085@gmail.com>
2025-12-25 22:24:11 +0530

    added custom method findByUserId in CodeSubmissionRepo

--- BACK-END/aicodereviwer/src/main/java/com/app/repository/CodeSubmissionRepo.java
+++ BACK-END/aicodereviwer/src/main/java/com/app/repository/CodeSubmissionRepo.java
@@ -1,9 +1,13 @@
 package com.app.repository;
 
 import com.app.entities.CodeSubmission;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface CodeSubmissionRepo extends JpaRepository<CodeSubmission, UUID> {
+
+    Page<CodeSubmission> findByUserId(Pageable pageable);
 }
